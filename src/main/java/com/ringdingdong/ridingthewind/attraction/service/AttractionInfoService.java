2b134faabbf54455a0a75fc04744ad513091506b
배용현 <byh9811@naver.com>
2023-04-30 21:29:21 +0900

    FEAT: Create AttractionInfo Service
    1. getAttractionInfoList 서비스 구현
    2. getMyAttractionInfoList 서비스 구현

---
+++ src/main/java/com/ringdingdong/ridingthewind/attraction/service/AttractionInfoService.java
@@ -0,0 +1,12 @@
+package com.ringdingdong.ridingthewind.attraction.service;
+
+import com.ringdingdong.ridingthewind.attraction.entity.AttractionInfo;
+
+import java.util.List;
+import java.util.Map;
+
+public interface AttractionInfoService {
+    List<AttractionInfo> getAttractionInfoList(Map<String, String> map);
+
+    List<AttractionInfo> getMyAttractionInfoList(String userId);
+}
---
+++ src/main/java/com/ringdingdong/ridingthewind/attraction/service/AttractionInfoServiceImpl.java
@@ -0,0 +1,27 @@
+package com.ringdingdong.ridingthewind.attraction.service;
+
+import com.ringdingdong.ridingthewind.attraction.entity.AttractionInfo;
+import com.ringdingdong.ridingthewind.attraction.mapper.AttractionInfoMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class AttractionInfoServiceImpl implements AttractionInfoService {
+
+    private final AttractionInfoMapper attractionInfoMapper;
+
+    @Override
+    public List<AttractionInfo> getAttractionInfoList(Map<String, String> map) {
+        return attractionInfoMapper.selectAttractionListBySidoAndGugun(map);
+    }
+
+    @Override
+    public List<AttractionInfo> getMyAttractionInfoList(String userId) {
+        return attractionInfoMapper.selectAttractionListByUserId(userId);
+    }
+
+}
