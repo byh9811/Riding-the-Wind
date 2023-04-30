ca6cbfaa2047605cfacb138537366becb8f2af16
배용현 <byh9811@naver.com>
2023-04-30 21:25:10 +0900

    FEAT: Create AttractionInfo Mapper
    1. 지역명으로 관광지 검색 Mapper 작성
    2. userId로 관광지 검색 Mapper 작성

---
+++ src/main/java/com/ringdingdong/ridingthewind/attraction/mapper/AttractionInfoMapper.java
@@ -0,0 +1,16 @@
+package com.ringdingdong.ridingthewind.attraction.mapper;
+
+import com.ringdingdong.ridingthewind.attraction.entity.AttractionInfo;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+import java.util.Map;
+
+@Mapper
+public interface AttractionInfoMapper {
+
+    List<AttractionInfo> selectAttractionListBySidoAndGugun(Map<String, String> map);
+
+    List<AttractionInfo> selectAttractionListByUserId(String userId);
+
+}
