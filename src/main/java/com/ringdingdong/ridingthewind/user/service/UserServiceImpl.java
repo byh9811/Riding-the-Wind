b7af5816811d7a1141ae6c922fbda883576e336a
유호재 <ho_0214@naver.com>
2023-05-01 18:52:34 +0900

    feat yuhojae userService temp1

--- src/main/java/com/ringdingdong/ridingthewind/user/service/UserServiceImpl.java
+++ src/main/java/com/ringdingdong/ridingthewind/user/service/UserServiceImpl.java
@@ -1,11 +1,13 @@
 package com.ringdingdong.ridingthewind.user.service;
 
 import com.ringdingdong.ridingthewind.user.controller.UserController;
+import com.ringdingdong.ridingthewind.user.mapper.UserMapper;
 import lombok.RequiredArgsConstructor;
+import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService{
-    public final UserController userController;
+    public final UserMapper userMapper;
 }
