296ecfc7abb2f7b4510fc3f052542cc2e47d0041
배용현 <byh9811@naver.com>
2023-05-01 10:46:11 +0900

    FIX: Update UserServiceImpl
    1. UserServiceImpl이 UserController를 Injection 받고있어서 수정함

--- src/main/java/com/ringdingdong/ridingthewind/user/service/UserServiceImpl.java
+++ src/main/java/com/ringdingdong/ridingthewind/user/service/UserServiceImpl.java
@@ -1,11 +1,12 @@
 package com.ringdingdong.ridingthewind.user.service;
 
 import com.ringdingdong.ridingthewind.user.controller.UserController;
+import com.ringdingdong.ridingthewind.user.mapper.UserMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService{
-    public final UserController userController;
+    public final UserMapper userMapper;
 }
