85e7ee989ca1dcb6130442ce4256cff5e1305744
yuhojae <ho_0214@naver.com>
2023-05-01 17:50:26 +0900

    feat yuhojae 로그인 기능 구현, user database connection complete

--- src/main/java/com/ringdingdong/ridingthewind/user/service/UserServiceImpl.java
+++ src/main/java/com/ringdingdong/ridingthewind/user/service/UserServiceImpl.java
@@ -1,6 +1,8 @@
 package com.ringdingdong.ridingthewind.user.service;
 
 import com.ringdingdong.ridingthewind.user.controller.UserController;
+import com.ringdingdong.ridingthewind.user.dto.UserDto;
+import com.ringdingdong.ridingthewind.user.entity.User;
 import com.ringdingdong.ridingthewind.user.mapper.UserMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,4 +11,9 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService{
     public final UserMapper userMapper;
+
+
+    public User signinUser(UserDto userDto) {
+        return userMapper.signinUser(userDto);
+    }
 }
